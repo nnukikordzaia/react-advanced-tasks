3fff12a9af6be9c70edfd1af5df9ea005afce66f
nnukikordzaia <anuki.kordzaia@azry.com>
2021-07-26 16:09:39 +0400

    added useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 
-function App(){
-    const [counter, setCounter] = useState(0);
-    const [incrementBy, setIncrement] = useState(1);
-
-    function increment(inc){
-        setCounter(counter + inc);
+function reducer(state, action){
+    switch(action.type){
+        case 'set': 
+            if( !action.incrementBy){
+                return{ counter:state.counter, value : 1}
+            }
+            return{ counter:state.counter, value : action.incrementBy}
+        case 'increment':
+            return{ counter: state.counter + state.value, value: state.value }
+        case 'decrement':
+            return{ counter: state.counter - state.value, value: state.value}
     }
+}
+function App(){
+    const initstate = {
+        counter: 0,
+        value: 1
+    };
 
+    const[state, dispatch] = useReducer(reducer, initstate)
+    // const [counter, setCounter] = useState(0);
+    // const [incrementBy, setIncrement] = useState(1);
+
+   
     return(
         <div> 
-            <input value={incrementBy}
-                onChange={e => setIncrement(Number(e.target.value))}/>
-            <button onClick={() => increment(incrementBy)}>+</button>
-            <span>{counter}</span>
-            <button onClick={() => increment(-incrementBy)}>-</button>
+            <input
+                
+                onChange={ (e) => dispatch({type: 'set', incrementBy: Number(e.target.value)})}/>
+            <button onClick={() => dispatch({type: 'increment'})}>+</button>
+            <span>{state.counter}</span>
+            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
         </div>
     );
 }
